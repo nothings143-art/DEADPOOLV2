9c93b045fbcdb37bc8a6240e1a8f8208bce6cff2
nothings143-art <rajpathi143@gmail.com>
2025-09-13 16:59:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://CYBERTG:CYBERTG@cybertg.sti1hiq.mongodb.net/?retryWrites=true&w=majority&appName=CyberTG")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "CyberTG")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Rkmovieszip')
 
 #stream link shortner
